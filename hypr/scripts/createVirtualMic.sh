800048b2e8d079d4b75e84e6a2d66da925648c5a
TheNofis <matbey122@gmail.com>
2023-06-16 03:03:54 +0700

    2 commite

---
+++ hypr/scripts/createVirtualMic.sh
@@ -0,0 +1,10 @@
+pactl load-module module-null-sink \
+    sink_name=audiorelay-virtual-mic-sink \
+    sink_properties=device.description=Virtual-Mic-Sink
+
+pactl load-module module-remap-source \
+    master=audiorelay-virtual-mic-sink.monitor \
+    source_name=audiorelay-virtual-mic-sink \
+    source_properties=device.description=Virtual-Mic
+
+/home/thenofis/programms/virtualMic/bin/AudioRelay
